cd6395d1835222805985ebde5195cd7cde20035c
Marcos Anzorena <anzorena@geofisica.unam.mx>
2020-01-25 01:56:25 -0600

    Update and rename tcp_daq.py to tcp-daq.py

--- tcp-daq.py
+++ tcp-daq.py
@@ -49,15 +49,15 @@ class socket_instrument(object):
     self.s.close()
 
 parser=argparse.ArgumentParser()
-parser.add_argument('nwav', help='numero de capturas',type=int)
+parser.add_argument('nwav', help='capture number',type=int)
 args=parser.parse_args()
 nwav=args.nwav
 
+# ip address need to be configure on the scope side
 scope=socket_instrument('192.168.10.14')
 
 wavenum=0
 
-#scope.write(':cou sta')
 scope.write(':head off')
 scope.write(':verb off')
 scope.write(':dat:sou ch1')
@@ -80,18 +80,15 @@ while wavenum < nwav:
   #scope.write(':acq:state run')
   wait=int(scope.ask(':*opc?'))
   ttrg=time.strftime('%H:%M:%S',time.localtime(time.time()))
-  scope.write(':curves?')   
-  data=scope.read()
-  print data
-  #data=np.array(scope.read()[5:-1],np.int8)
-  print 'ok'
-  #y=yfac*(data-yoff)
+  scope.write(':curv?')   
+  data=np.array(scope.read()[5:-1],np.int8)
+  y=yfac*(data-yoff)
   wavenum+=1
-  #fdat.write('FC: {0}\n'.format(ttrg))
-  #np.savetxt(fdat,y,fmt='%1.4f',newline=' ')
-  #fdat.write('\n')
+  fdat.write('FC: {0}\n'.format(ttrg))
+  np.savetxt(fdat,y,fmt='%1.4f',newline=' ')
+  fdat.write('\n')
 
 t1=time.time()
-print u'Adquisición completa en: {0} hrs.'.format(str(datetime.timedelta(seconds=(t1-t0)))[0:7])
+print u'Data acqusition completed in: {0} hours.'.format(str(datetime.timedelta(seconds=(t1-t0)))[0:7])
 print nwav/(t1-t0)
 scope.close()
